788498cb7ac9dd3573dfd316994a93a433383d3b
atefganm <atef_anim_1974@yahoo.com>
2024-07-17 22:01:29 +0300

    Update-4

--- lib/gdi/fblcd.cpp
+++ lib/gdi/fblcd.cpp
@@ -11,12 +11,6 @@
 #define PNG_SKIP_SETJMP_CHECK
 #include <png.h>
 
-#ifdef HAVE_OLDE2_API
-#ifndef FBIO_WAITFORVSYNC
-#define FBIO_WAITFORVSYNC _IOW('F', 0x20, uint32_t)
-#endif
-#endif
-
 #ifndef FBIO_BLIT
 #define FBIO_SET_MANUAL_BLIT _IOW('F', 0x21, __u8)
 #define FBIO_BLIT 0x22
@@ -316,7 +310,7 @@ void eFbLCD::dumpLCD(bool png=true)
 void eFbLCD::save2png(unsigned char* output, int xRes, int yRes)
 {
 	int output_bytes=4; //osd only
-	const char* filename = "/tmp/lcdshot.png";
+	const char* filename = "/tmp/lcd.png";
 
 	FILE *fd2;
 	fd2 = fopen(filename, "wr");
@@ -354,7 +348,7 @@ void eFbLCD::save2png(unsigned char* output, int xRes, int yRes)
 void eFbLCD::save2bmp(unsigned char* output, int xRes, int yRes)
 {
 	int output_bytes=4;  //osd only
-	const char* filename = "/tmp/lcdshot.bmp";
+	const char* filename = "/tmp/lcd.bmp";
 
 	FILE *fd2;
 	fd2 = fopen(filename, "wr");
