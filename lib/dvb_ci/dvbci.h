3c1ff3d8386f4244f3cc1d518e7319d74b6d2f98
atefganm <atef_anim_1974@yahoo.com>
2024-07-19 19:36:08 +0300

    Update

--- lib/dvb_ci/dvbci.h
+++ lib/dvb_ci/dvbci.h
@@ -111,8 +111,10 @@ class eDVBCISlot: public iObject, public sigc::trackable
 	int setClockRate(int);
 	void determineCIVersion();
 	int setEnabled(bool);
-public:
 	static std::string getTunerLetter(int tuner_no) { return std::string(1, char(65 + tuner_no)); }
+	static std::string getTunerLetterDM(int);
+	static char* readInputCI(int);
+public:
 	enum {stateRemoved, stateInserted, stateInvalid, stateResetted, stateDisabled};
 	enum {versionUnknown = -1, versionCI = 0, versionCIPlus1 = 1, versionCIPlus2 = 2};
 	eDVBCISlot(eMainloop *context, int nr);
